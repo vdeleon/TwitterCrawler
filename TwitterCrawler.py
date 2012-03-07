a6c801613f129c55e10acbe1a9223196fe256c56
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2012-03-07 19:33:40 +0100

    Main method and frontend skeleton

--- TwitterCrawler.py
+++ TwitterCrawler.py
@@ -3,5 +3,13 @@ Created on 15/feb/2012
 
 @author: Riccardo Ferrazzo <f.riccardo87@gmail.com>
 '''
+import sys
+from PySide.QtCore import *
+from frontend.MainWindow import *
 
-        
+app = QApplication(sys.argv)
+win = MainWindow()
+win.show()
+
+app.exec_()
+sys.exit()        
