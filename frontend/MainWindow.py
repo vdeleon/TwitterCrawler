87dc34f1d0144b1225eea68ee6e811264968c93c
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2012-03-07 19:33:53 +0100

    Main method and frontend skeleton

---
+++ frontend/MainWindow.py
@@ -0,0 +1,12 @@
+'''
+Created on 07/mar/2012
+
+@author: Riccardo Ferrazzo <f.riccardo87@gmail.com>
+'''
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
