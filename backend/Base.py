90e6f211b307a2285c3535c69d78f8ee297e61dd
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2012-04-10 19:23:57 +0200

    added LGPL licence

--- backend/Base.py
+++ backend/Base.py
@@ -1,7 +1,21 @@
 '''
-Created on 09/mar/2012
+Copyright (C) 2012 Riccardo Ferrazzo <f.riccardo87@gmail.com>
 
-@author: Riccardo Ferrazzo <f.riccardo87@gmail.com>
+This file is part of TwitterCrawler.
+
+    TwitterCrawler is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TwitterCrawler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TwitterCrawler.  If not, see <http://www.gnu.org/licenses/>
+    
 '''
 from PySide.QtCore import *
 
--- frontend/frontend.qml
+++ frontend/frontend.qml
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2012 Riccardo Ferrazzo <f.riccardo87@gmail.com>
+
+This file is part of TwitterCrawler.
+
+    TwitterCrawler is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    TwitterCrawler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with TwitterCrawler.  If not, see <http://www.gnu.org/licenses/>
+
+*/
 // import QtQuick 1.0 // to target S60 5th Edition or Maemo 5
 import QtQuick 1.1
 import TwitterCrawler 1.0
